a3f233cca888a98863ebaa493a623fd81c92752e
cbizon <bizon@renci.org>
2020-04-15 09:37:51 -0400

    fixed

--- Normalize.py
+++ Normalize.py
@@ -1,23 +1,141 @@
 import os
 import requests
 import sys
+from collections import defaultdict
+
+#These are curies that tend to give a lot of false positives, or are generally
+# too general to be of interest
+garbage_curies = set(['MONDO:0019395', #Hinman syndrome with synonym "HAS",
+                      'MONDO:0012833', #"CAN"
+                      'MONDO:0000001', #disease
+                      'MONDO:0004967', #"ALL"
+                      'UBERON:0014899', #"ALL",
+                      'HP:0000001',
+                      'UBERON:0006611', #Has synonym test
+                      'ENVO:00000026', #well
+                      'CHEBI:33232', #application
+                      'CHEBI:75958', #solution
+                      'ENVO:01000584', #table
+                      'ENVO:2000036', #generates
+                      'HP:0012824', #severity
+                      'HP:0012830', #Position
+                      'HP:0012834', #Right
+                      'HP:0012835', #Left
+                      'HP:0032320', #Affected
+                      'HP:0040279', #Frequency
+                      'HP:0040285', #Excluded
+                      'HP:0012832', #Bilateral
+                      'CHEBI:24433', #Group
+                      'CHEBI:52217', #Pharmaceutical
+                      'CHEBI:23888', #Drug
+                      'CHEBI:50906', #Role
+                      'HP:0011009', #Acute
+                      'HP:0012828', #Severe
+                      'HP:0025254', #Ameliorated by
+                      'HP:0032322', #Healthy
+                      'MONDO:0021137', #not rare
+                      'NCBITaxon:order',
+                      'NCBITaxon:family',
+                      'CHEBI:35225', #buffer
+                      'CHEBI:60004', #mixture
+                      'CHEBI:25367', #molecule
+                      'GO:0005623', #cell
+                      'FOODON:03420236', #foodon
+                      'ENVO:01000605', #car
+                      'FOODON:00003004', #animal
+                      'GO:0005488', #binding
+                      'HP:0012825', #Mild
+                      'NCBIGene:4233', #MET
+                      'NCBIGene:55364', #IMPACT
+                      'NCBIGene:6418', #SET
+                      'HP:0030646', #Peripheral
+                      'HP:0040282', #Frequent
+                      'NCBIGene:94005', #PIGS
+                      'HP:0003674', #Onset
+                      'NCBIGene:3815', #KIT
+                      'NCBIGene:4280', #MICE
+                      'UBERON:0004529', #anatomical projection
+                      'HP:0011010', #Chronic
+                      'CHEBI:33893', #reagent
+                      'MONDO:0045042', #localized
+                      'FOODON:03430131', #whole
+                      'HP:0025303', #episodic
+                      'HP:0003745', #Sporadic
+                      'HP:0003676', #Progressive
+                      'HP:0025275', #Lateral
+                      'HP:0020034', #Diffuse
+                      'HP:0031797', #Clinical course
+                      'HP:0012833', #Unilateral
+                      'CHEBI:75830', #HOME
+                      'CHEBI:33731', #cluster
+                      'NCBITaxon:class',
+                      'NCBITaxon:species',
+                      'MONDO:0021141', #acquired
+                      'ENVO:00000444', #clearing
+                      'ENVO:00000427', #meander
+                      'CHEBI:33250', #atom
+                      'ENVO:01000604', #vehicle
+                      'HP:0025297', #Prolongued
+                      'HP:0012840', #Proximal
+                      'ENVO:01000588', #sofa
+                      'CHEBI:3608', #protein
+                      'ENVO:00000480', #peak
+                      'HP:0012829', #Profound
+                      'HP:0025285', #Aggravated by
+                      ])
+
+
+#We're not incorporating any of these at the moment
+unused_prefixes = ['DDANAT', 'UO', 'SO', 'RO', 'EFO', 'PR', 'MF', 'OBI', 'VO',
+                   'BFO', 'PATO', 'IAO', 'BSPO', 'IDO', 'BTO', 'http', 'TERMS',
+                   'OGMS', 'FMA', 'MP', 'MPATH', 'NBO', 'MA', 'UBPROP', 'UBREL',
+                   'CARO', 'PCO', 'OBA', 'CLO', 'EO', 'FBBT', 'CHMO', 'SIO', 'OMIT',
+                   'CP', 'GAZ', 'ORPHANET', 'TO', 'OPL', 'ZFA', 'NCIT', 'ZEA', 'MOD',
+                   'MGI', 'REO', 'OMIABIS', 'GENE', 'WBLS','DEPICTED']
 
 class Normalizer():
     def __init__(self):
+        """Class for converting ontology / vocabulary terms from scigraph into Translator-compliant entities.
+            Input terms are added to the normalizer using 'add'.  Once all terms are added, 'normalize_all' calls
+            the Translator node-normalization service in batches (for efficiency).   Additionally, there are some
+            special cases to handle identifiers that are not found in nodenorm, and to remove some identifiers that
+            are often false positives."""
         self.url = 'https://nodenormalization-sri.renci.org/get_normalized_nodes'
+        self.curie_to_iri = {}
         self.iri_to_curie = {}
         self.iri_to_label = {}
         self.curie_to_normalized = {}
         self.curie_to_type = {}
         self.curie_to_label = {}
     def add(self,iri,label):
+        """Add a new IRI and label to the normalizer"""
+        useless_iri = set()
         if iri in self.iri_to_curie:
             return
-        newcurie = self.curify(iri)
+        if iri in useless_iri:
+            return
+        newcurie = self._curify(iri)
+        if newcurie is None:
+            useless_iri.add(iri)
+            return
+        if '#' in newcurie:
+            print ("how did I get here?")
+            print(iri)
+            exit()
         self.iri_to_curie[iri] = newcurie
         self.iri_to_label[iri] = label
-    def curify(self,iri):
-        #print(iri)
+        self.curie_to_iri[newcurie] = iri
+    def _curify(self,iri):
+        """Convert IRI to curie"""
+        #These don't convert nicely, junk them
+        if iri.startswith('http://dbpedia.org/resource/') or \
+           iri.startswith('http://purl.org/dc/elements/') or \
+           iri.startswith('http://purl.org/dc/terms/') or \
+           iri.startswith('http://xmlns.com/foaf/') or \
+           iri.startswith('http://flybase.org/') or \
+           iri.startswith('http://www.informatics.jax.org/') or \
+           iri.startswith('https://www.wikidata.org/'):
+           return None
         ns = iri.split('/')
         n = ns[-1]
         if n.startswith('gene_symbol_report'):
@@ -25,14 +143,32 @@ class Normalizer():
         if ':' in n:
             return n
         x = n.split('_')
+        c = None
         if len(x) == 2:
             x[0] = x[0].upper()
-            return ':'.join(x)
+            if x[0] in unused_prefixes:
+                return None
+            c = ':'.join(x)
         if len(x) == 1:
-            return f'{ns[-2].upper()}:{ns[-1]}'
-        print(iri)
-        return(iri)
+            prefix = ns[-2].upper()
+            if prefix in unused_prefixes:
+                return None
+            c = f'{prefix}:{ns[-1]}'
+        if c is not None:
+            if '#' in c:
+                return None
+            return c
+        return None
+    def normalize(self,iri):
+        try:
+            c = self.iri_to_curie[iri]
+            n = self.curie_to_normalized[c]
+            return n
+        except:
+            return None
     def normalize_all(self):
+        """Call node normalization on batches of curies, and convert into their
+        translator-preferred forms"""
         batchsize=20
         unnormalized_curies = self.iri_to_curie.values()
         print(len(unnormalized_curies))
@@ -41,17 +177,94 @@ class Normalizer():
         for x in unnormalized_curies:
             batch.append(x)
             if len(batch) >= batchsize:
-                batchmap,typemap,labelmap = self.normalize_batch(batch)
+                batchmap,typemap,labelmap = self._normalize_batch(batch)
                 self.curie_to_normalized.update(batchmap)
                 self.curie_to_type.update(typemap)
                 self.curie_to_label.update(labelmap)
                 batch= []
         if len(batch) > 0:
-            batchmap,typemap,labelmap = self.normalize_batch(batch)
+            batchmap,typemap,labelmap = self._normalize_batch(batch)
             self.curie_to_normalized.update(batchmap)
             self.curie_to_type.update(typemap)
             self.curie_to_label.update(labelmap)
-    def normalize_batch(self,batch):
+        self._remove_obsolete_terms()
+        self._remove_garbage()
+        #It's possible here that we have an iri that got turned into a curie,
+        # and we sent it to normalization, and it didn't normalize, and we end
+        # up with no type labels.  Need to fix that
+        self._fix_type_labels()
+    def _fix_type_labels(self):
+        prefix_to_types = {'MONDO':['disease', 'named_thing', 'biological_entity', 'disease_or_phenotypic_feature'],
+        'EFO': ['phenotypic_feature', 'named_thing', 'biological_entity', 'disease_or_phenotypic_feature'],
+        'ECTO': ['chemical_exposure', 'exposure_event', 'named_thing'],
+        'ENVO': ['environmental_feature', 'planetary_entity', 'named_thing'],
+        'HANCESTRO': ['population_of_individual_organisms', 'named_thing'],
+        'FAO':['anatomical_entity', 'organismal_entity', 'biological_entity', 'named_thing'],
+        'PO':['anatomical_entity', 'organismal_entity', 'biological_entity', 'named_thing'],
+        'CHEBI':['chemical_substance', 'molecular_entity', 'biological_entity', 'named_thing'],
+        'HsapDv': ['life_stage','organismal_entity','named_thing'],
+        'FOODON':['food', 'named_thing'] }
+        for gc,types in self.curie_to_type.items():
+            if len(types) == 1:
+                if types[0] != 'named_thing':
+                    print('seems wrong')
+                    exit()
+                normcurie = self.curie_to_normalized[gc]
+                prefix = normcurie.split(':')[0]
+                if prefix in prefix_to_types:
+                    self.curie_to_type[gc] = prefix_to_types[prefix]
+                elif prefix in ['UBERON','HP','GO','CL','NCBITAXON']:
+                    #This is a normalized identifier that is obsolete, or otherwise poor.
+                    # Get rid of it!
+                    iri = self.curie_to_iri[gc]
+                    del self.iri_to_curie[iri]
+                elif prefix == 'CHEBI':
+                    # hardcoding some conversions to get around issues with current nn
+                    chebis = {'CHEBI:26158': 'CHEBI:37848',
+                              'CHEBI:22584': 'CHEBI:2762',
+                              'CHEBI:185922': 'CHEBI:40154',
+                              'CHEBI:2360': 'CHEBI:421707',
+                              'CHEBI:153671': 'CHEBI:80240',
+                              'CHEBI:64208': 'CHEBI:90693',
+                              'CHEBI:4836': 'CHEBI:28792',
+                              'CHEBI:33097': 'CHEBI:35227'}
+                    self.curie_to_normalized[gc] = chebis[gc]
+                    self.curie_to_type[gc] = prefix_to_types['CHEBI']
+                elif prefix == 'HSAPDV':
+                    p = gc.split(':')
+                    self.curie_to_normalized[gc] = f'HsapDv:{p[1]}'
+                    self.curie_to_type[gc] = prefix_to_types['HsapDv']
+                else:
+                    print('?')
+                    print(prefix)
+                    print(gc)
+                    print(normcurie)
+                    print(self.curie_to_iri[gc])
+                    exit()
+    def _remove_obsolete_terms(self):
+        # If the label is "obsolete" something or another, we want to get rid of it...
+        bad_curies = set()
+        for curie,label in self.curie_to_label.items():
+            if label.startswith('obsolete'):
+                bad_curies.add(curie)
+        for bc in bad_curies:
+            del self.curie_to_normalized[bc]
+            del self.curie_to_type[bc]
+            del self.curie_to_label[bc]
+    def _remove_garbage(self):
+        #The garbage curies are normalized, so I need to swap
+        n2c = defaultdict(list)
+        for c,n in self.curie_to_normalized.items():
+            n2c[n].append(c)
+        for gc in garbage_curies:
+            for c in n2c[gc]:
+                del self.curie_to_normalized[c]
+                del self.curie_to_type[c]
+                try:
+                    del self.curie_to_label[c]
+                except KeyError:
+                    continue
+    def _normalize_batch(self,batch):
         result = requests.get('https://nodenormalization-sri.renci.org/get_normalized_nodes', params={'curie': batch})
         r = result.json()
         nb = {}
@@ -61,7 +274,7 @@ class Normalizer():
             back = r[b]
             if r[b] is None:
                 nb[b] = b
-                types[b] = '[named_thing]'
+                types[b] = ['named_thing']
             else:
                 bid = r[b]['id']['identifier']
                 try:
@@ -72,19 +285,24 @@ class Normalizer():
                 types[b] = r[b]['type']
         return nb,types,labels
     def write(self,ofile):
+        """Write normalized entities to a file"""
+        print(f'writing to {ofile}')
         with open(ofile, 'w') as outf:
-            outf.write('input_term\tcurie\tnormalized_curie\tsemantic_type\tlabel\n')
+            outf.write('normalized_curie\tsemantic_type\tlabel\n')
             for iri,curie in self.iri_to_curie.items():
-                normcurie = self.curie_to_normalized[curie]
-                normtype = self.curie_to_type[curie]
-                if curie in self.curie_to_label:
-                    label = self.curie_to_label[curie]
-                else:
-                    label = self.iri_to_label[iri]
-                outf.write(f'{iri}\t{curie}\t{normcurie}\t{normtype}\t{label}\n')
+                try:
+                    normcurie = self.curie_to_normalized[curie]
+                    normtype = self.curie_to_type[curie]
+                    if curie in self.curie_to_label:
+                        label = self.curie_to_label[curie]
+                    else:
+                        label = self.iri_to_label[iri]
+                    outf.write(f'{normcurie}\t{normtype}\t{label}\n')
+                except KeyError:
+                    continue
 
 
-def work_on_rfiles(indir,outdir):
+def normalize(indir,outdir):
     normy = Normalizer()
     rfiles = os.listdir(indir)
     #for rf in rfiles[:1]:
@@ -98,6 +316,19 @@ def work_on_rfiles(indir,outdir):
                 normy.add(term,label)
     normy.normalize_all()
     normy.write(f'{outdir}/normalized.txt')
+    papers = set()
+    for i,rf in enumerate(rfiles):
+        with open(f'{indir}/{rf}','r') as inf, open(f'{outdir}/annotation_{i}.txt','w') as outf:
+            outf.write('Curie\tPaper\n')
+            for line in inf:
+                x = line.strip().split('\t')
+                pmid = x[0]
+                papers.add(pmid)
+                term = x[6]
+                curie = normy.normalize(term)
+                if curie is not None:
+                    outf.write(f'{curie}\t{pmid}\n')
+    return len(papers)
 
 if __name__ == '__main__':
-    work_on_rfiles(sys.argv[1],sys.argv[2])
+    normalize('input','output')
--- co_occur.py
+++ co_occur.py
@@ -1,49 +1,35 @@
 import os
+from scipy.stats import hypergeom
 from collections import defaultdict
 
-def load_norms():
-    norms = {}
-    names = {}
-    with open('output/renormalized.txt','r') as inf:
-        header = inf.readline()
-        for line in inf:
-            x = line.strip().split('\t')
-            iri = x[0]
-            norm = x[2]
-            name = x[-1]
-            norms[iri] = norm
-            names[norm] = name
-    return norms,names
-
-def work_on_rfiles(indir,outdir):
-    norms,names = load_norms()
+def make_pairs(wdir,total_paper_count,p_cutoff=1e-7):
+    relevance = 1. / 30.
     terms_to_papers = defaultdict(set)
-    rfiles = os.listdir(indir)
+    rfiles = os.listdir(wdir)
     for rf in rfiles:
-        with open(f'{indir}/{rf}','r') as inf:
-            for line in inf:
-                x = line.strip().split('\t')
-                pmid = x[0]
-                term = x[6]
-                try:
-                    nterm = norms[term]
-                    terms_to_papers[nterm].add(pmid)
-                except KeyError:
-                    #this is a crappy identifier, let's just keep going
-                    pass
+        if rf.startswith('annotation'):
+            with open(f'{wdir}/{rf}','r') as inf:
+                h = inf.readline()
+                for line in inf:
+                    x = line.strip().split('\t')
+                    term = x[0]
+                    pmid = x[1]
+                    terms_to_papers[term].add(pmid)
     terms = list(terms_to_papers.keys())
-    with open(f'{outdir}/pairs.txt','w') as outf:
-        outf.write('Term1\tTerm2\tLabel1\tLabel2\tCountTerm1\tCountTerm2\tSharedCount\n')
-        for i,t_1 in enumerate(terms):
-            for t_2 in terms[i+1:]:
-                s1 = terms_to_papers[t_1]
-                s2 = terms_to_papers[t_2]
-                pair = s1.intersection(s2)
-                if len(pair) > 0:
-                    label1 = names[t_1]
-                    label2 = names[t_2]
-                    outf.write(f'{t_1}\t{t_2}\t{label1}\t{label2}\t{len(s1)}\t{len(s2)}\t{len(pair)}\n')
-
-if __name__ == '__main__':
-    work_on_rfiles('input', 'output')
+    with open(f'{wdir}/pairs.txt','w') as outf:
+        outf.write('Term1\tTerm2\tCountTerm1\tCountTerm2\tSharedCount\tEnrichment_p\tEffective_Pubs\n')
+        for i,Term1 in enumerate(terms):
+            for Term2 in terms[i+1:]:
+                terms1 = terms_to_papers[Term1]
+                terms2 = terms_to_papers[Term2]
+                SharedCount = len(terms1.intersection(terms2))
+                CountTerm1 = len(terms1)
+                CountTerm2 = len(terms2)
+                if SharedCount > 0:
+                    enrichp = hypergeom.sf(SharedCount - 1, total_paper_count, CountTerm2, CountTerm1)
+                    if enrichp < p_cutoff:
+                        cov = (SharedCount / total_paper_count) - (CountTerm1 / total_paper_count) * (CountTerm2 / total_paper_count)
+                        cov = max((cov, 0.0))
+                        effective_pubs = cov * total_paper_count * relevance
+                        outf.write(f'{Term1}\t{Term2}\t{CountTerm1}\t{CountTerm2}\t{SharedCount}\t{enrichp}\t{effective_pubs}\n')
 
